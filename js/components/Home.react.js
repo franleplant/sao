15b278175572c373b0b91fcf858724f62ef06f63
Fran Guijarro <franleplant@gmail.com>
2015-04-14 17:15:34 -0300

    erase console log

--- js/components/Home.react.js
+++ js/components/Home.react.js
@@ -6,7 +6,6 @@ import sessionStore from '../stores/sessionStore.js';
 export default class Home extends React.Component {
     static willTransitionTo(transition) {
         // This method is called before transitioning to this component. If the user is not logged in, we’ll send him or her to the Login page.
-        console.log('transitioning to home')
         if (!sessionStore.isLoggedIn()) {
             transition.redirect('/login');
         }
--- js/stores/sessionStore.js
+++ js/stores/sessionStore.js
@@ -42,4 +42,4 @@ class SessionStore {
     }
 }
 
-export default new SessionStore(); 
+export default new SessionStore();
